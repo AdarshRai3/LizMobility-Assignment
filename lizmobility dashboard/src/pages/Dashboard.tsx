95e8a673e1694ab95eda911c11ab1a3509eb0e9e
AdarshRai3 <adarshrai321999@gmail.com>
2024-03-21 15:29:00 +0530

    "Complete dashboard"

--- lizmobility dashboard/src/pages/Dashboard.tsx
+++ lizmobility dashboard/src/pages/Dashboard.tsx
@@ -2,7 +2,7 @@ import AdminSidebar from "../components/AdminSidebar";
 import { BsSearch } from "react-icons/bs";
 import { FaRegBell } from "react-icons/fa";
 import userPic from "../assets/profile3478223ba4ea84756140387043a66fc5f0ed3e6.webp";
-
+import Flow from "../components/Flow";
 const Dashboard = () => {
   return (
     <div className = "admin-container">
@@ -14,6 +14,12 @@ const Dashboard = () => {
           <input type="test" placeholder="Search for projects"/>
           <FaRegBell/>
           <img src={userPic} alt="user"/>
+        </div>
+        <div className="content">
+          <h3>Project Dashboard</h3>
+          <div className="flow">
+            <Flow/>
+          </div>
         </div>
       </main> 
     </div>
